f086370a98775d83b6738227d8739effb627a77d
BenAllenUK <external@benallen.info>
2015-03-07 13:55:46 +0000

    Added group maker fragment in seperate class and animation

--- app/src/main/java/com/dbuggers/flare/MainActivity.java
+++ app/src/main/java/com/dbuggers/flare/MainActivity.java
@@ -1,20 +1,16 @@
 package com.dbuggers.flare;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
-import android.widget.Button;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements SignupFragment.SignupInterface, GroupMakerFragment.GroupMakerInterface {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,48 +18,21 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new SignupFragment())
                     .commit();
         }
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-            final Button button = (Button) rootView.findViewById(R.id.signUpBtn);
-            button.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    // Perform action on click
-                    Log.v("Ah","Button Press");
-                }
-            });
-
-            return rootView;
-        }
-    }
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class GroupMaker extends Fragment {
-
-        public GroupMaker() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_creategroup, container, false);
-            return rootView;
-        }
+    @Override
+    public void onSignupClicked() {
+        Log.v("ah", "signup clicked");
+        /*getFragmentManager().beginTransaction()
+                .setCustomAnimations(android.R.anim.slide_in_left, android.R.anim.slide_out_right)
+                .replace(R.id.container, new GroupMakerFragment())
+                .commit();*/
+        getFragmentManager().beginTransaction()
+                .setTransition( FragmentTransaction.TRANSIT_FRAGMENT_FADE )
+                .replace(R.id.container, new GroupMakerFragment())
+                .commit();
     }
 }
---
+++ app/src/main/java/com/dbuggers/flare/SignupFragment.java
@@ -0,0 +1,53 @@
+package com.dbuggers.flare;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * Created by benallen on 07/03/15.
+ */
+public class SignupFragment extends Fragment {
+
+    private SignupInterface signupInterface;
+
+    public interface SignupInterface {
+        public void onSignupClicked();
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        if(getActivity() instanceof SignupInterface){
+            signupInterface = (SignupInterface) getActivity();
+        }else{
+            throw new IllegalStateException(getActivity().getClass().getName()+" must implement SignupInterface");
+        }
+    }
+
+    public SignupFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        final Button button = (Button) rootView.findViewById(R.id.signUpBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Log.v("Ah", "Button Press");
+                signupInterface.onSignupClicked();
+
+            }
+        });
+
+        return rootView;
+    }
+}
